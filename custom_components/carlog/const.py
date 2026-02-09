c074e238cced52f4ccd3741f53bf9d75fec2b578
Sven Koppendraijer <sven@svenkopp.com>
2026-02-09 12:37:24 +0100

    Update tire maintenance interval values

--- custom_components/carlog/const.py
+++ custom_components/carlog/const.py
@@ -4,6 +4,6 @@ STORAGE_VERSION = 1
 
 DEFAULT_MAINTENANCE_TYPES = {
     "oil": {"label": "Olie", "interval_km": 15000, "interval_days": 365},
-    "tires": {"label": "Banden wissel", "interval_km": 30000, "interval_days": 365},
+    "tires": {"label": "Banden wissel", "interval_km": 35000, "interval_days": 2136},
     "brakes": {"label": "Remmen", "interval_km": 60000, "interval_days": 730},
 }
